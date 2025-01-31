8376051976e95d197faf087b47e49b4be140484a
alex3195 <alex9954161@mail.ru>
2025-01-31 13:39:51 +0500

    feat(init project with configs): init project with configs
    init project with configs

---
+++ src/main/java/uz/duol/ecopharmwarehouse/EcoPharmWarehouseApplication.java
@@ -0,0 +1,13 @@
+package uz.duol.ecopharmwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoPharmWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoPharmWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/duol/ecopharmwarehouse/EcoPharmWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.duol.ecopharmwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoPharmWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
