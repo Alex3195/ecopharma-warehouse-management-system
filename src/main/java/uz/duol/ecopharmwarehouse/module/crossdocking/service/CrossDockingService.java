657ba34a98ba6bbf156dde78173119a9445b73e4
alex3195 <alex9954161@mail.ru>
2025-05-15 11:50:16 +0500

    feat(crossdocking): add transactional support to CrossDockingService methods

--- src/main/java/uz/duol/ecopharmwarehouse/module/crossdocking/service/CrossDockingService.java
+++ src/main/java/uz/duol/ecopharmwarehouse/module/crossdocking/service/CrossDockingService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uz.duol.ecopharmwarehouse.entity.CrossDockingEntity;
 import uz.duol.ecopharmwarehouse.enums.Status;
 import uz.duol.ecopharmwarehouse.module.crossdocking.dto.CrossDockingDto;
@@ -20,18 +21,21 @@ public class CrossDockingService {
     private final CrossDockingRepository crossDockingRepository;
     private final CrossDockingMapper crossDockingMapper;
 
+    @Transactional
     public CrossDockingDto create(CrossDockingDto crossDockingDto) {
         var entity = crossDockingMapper.toEntity(crossDockingDto);
         var savedEntity = crossDockingRepository.save(entity);
         return crossDockingMapper.toDto(savedEntity);
     }
 
+    @Transactional(readOnly = true)
     public CrossDockingDto findById(Long id) {
         var entity = crossDockingRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Cross docking not found"));
         return crossDockingMapper.toDto(entity);
     }
 
+    @Transactional
     public CrossDockingDto update(Long id, CrossDockingDto crossDockingDto) {
         var entity = crossDockingRepository.findByIdAndStatusIsNot(id, Status.DELETED)
                 .orElseThrow(() -> new EntityNotFoundException("Cross docking not found"));
@@ -40,6 +44,7 @@ public class CrossDockingService {
         return crossDockingMapper.toDto(entity);
     }
 
+    @Transactional
     public void delete(Long id) {
         var entity = crossDockingRepository.findByIdAndStatusIsNot(id, Status.DELETED)
                 .orElseThrow(() -> new EntityNotFoundException("Cross docking not found"));
@@ -47,6 +52,7 @@ public class CrossDockingService {
         crossDockingRepository.save(entity);
     }
 
+    @Transactional(readOnly = true)
     public Page<CrossDockingDto> findAll(Pageable pageable) {
         Specification<CrossDockingEntity> spec = CrossDockingSpecification.isActive();
         return crossDockingRepository.findAll(spec, pageable)
