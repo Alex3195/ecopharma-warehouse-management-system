0bdd6db8954140d1632e163330ef5bfda659fd8e
DAVRON <abdusalomovdavron3703@gmail.com>
2025-08-06 13:10:43 +0500

    Addition:   CustomerSupplier filters added.

--- src/main/java/uz/duol/ecopharmwarehouse/module/customersupplier/specification/CustomerSupplierSpecification.java
+++ src/main/java/uz/duol/ecopharmwarehouse/module/customersupplier/specification/CustomerSupplierSpecification.java
@@ -11,8 +11,36 @@ public class CustomerSupplierSpecification {
         return ((root, query, criteriaBuilder) -> criteriaBuilder.notEqual(root.get("status"), Status.DELETED));
     }
 
+    public static Specification<CustomerSupplierEntity> hasFirstname(String firstname) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(criteriaBuilder.lower(root.get("firstName")), "%" + firstname.toLowerCase().trim() + "%");
+    }
+
+    public static Specification<CustomerSupplierEntity> hasLastname(String lastname) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(criteriaBuilder.lower(root.get("lastName")), "%" + lastname.toLowerCase().trim() + "%");
+    }
+
+    public static Specification<CustomerSupplierEntity> hasPhone(String phone) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(criteriaBuilder.lower(root.get("phone")), "%" + phone.toLowerCase().trim() + "%");
+    }
+
+
     public static Specification<CustomerSupplierEntity> advancedFilter(Map<String, Object> filters) {
         Specification<CustomerSupplierEntity> spec = isActive();
+
+        if (filters.containsKey("firstName")) {
+            spec = spec.and(hasFirstname((String) filters.get("firstName")));
+        }
+
+        if (filters.containsKey("lastName")) {
+            spec = spec.and(hasLastname((String) filters.get("lastName")));
+        }
+
+        if (filters.containsKey("phone")) {
+            spec = spec.and(hasPhone((String) filters.get("phone")));
+        }
         return spec;
     }
 }
